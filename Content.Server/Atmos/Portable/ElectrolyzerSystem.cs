33b5ac3e9d59cef18c0a055a03a01dc178629a58
marc-pelletier <113944176+marc-pelletier@users.noreply.github.com>
2025-04-10 15:14:28 -0400

    Add remaining /tg/ gases (#614)
    Co-authored-by: Steve <marlumpy@gmail.com>

--- Content.Server/Atmos/Portable/ElectrolyzerSystem.cs
+++ Content.Server/Atmos/Portable/ElectrolyzerSystem.cs
@@ -1,18 +1,14 @@
 // Assmos - /tg/ gases
 using Content.Server.Atmos.EntitySystems;
 using Content.Server.Atmos.Piping.Components;
-using Content.Server.Atmos.Piping.Unary.Components;
 using Content.Server.Popups;
 using Content.Server.Power.Components;
 using Content.Server.Power.EntitySystems;
-using Content.Shared.Atmos.Piping.Portable.Components;
 using Content.Shared.Atmos.Visuals;
 using Content.Shared.Power;
-using Content.Shared.UserInterface;
 using Robust.Server.GameObjects;
 using Content.Shared.Interaction;
 using Content.Shared.Atmos;
-using Content.Server.Atmos.Components;
 
 namespace Content.Server.Atmos.Portable;
 
@@ -22,7 +18,6 @@ public sealed class ElectrolyzerSystem : EntitySystem
     [Dependency] private readonly PopupSystem _popup = default!;
     [Dependency] private readonly PowerReceiverSystem _power = default!;
     [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
-    [Dependency] private readonly UserInterfaceSystem _userInterfaceSystem = default!;
     [Dependency] private readonly GasTileOverlaySystem _gasOverlaySystem = default!;
 
     public override void Initialize()
@@ -79,23 +74,52 @@ public sealed class ElectrolyzerSystem : EntitySystem
         if (mixture is null) return;
 
         var initH2O = mixture.GetMoles(Gas.WaterVapor);
+        var initHyperNob = mixture.GetMoles(Gas.HyperNoblium);
+        var initBZ = mixture.GetMoles(Gas.BZ);
+        var temperature = mixture.Temperature;
 
-        if (initH2O < 0.05f) return; 
+        if (initH2O > 0.05f)
+        {
+            var proportion = Math.Min(initH2O * 0.5f, 2.5f);
+            var efficiency = Math.Min(mixture.Temperature / 1123.15f * 0.75f, 0.75f);
 
-        var porportion = Math.Min(initH2O * 0.5f, 2.5f);
-        var efficiency = Math.Min(mixture.Temperature / 1123.15f, 1f);
+            var h2oRemoved = proportion * 2f;
+            var oxyProduced = proportion * efficiency;
+            var hydrogenProduced = proportion * 2f * efficiency;
 
-        var h2oRemoved = porportion * 2f * efficiency;
-        var oxyProduced = porportion * efficiency;
-        var hydrogenProduced = porportion * 2f * efficiency;
+            mixture.AdjustMoles(Gas.WaterVapor, -h2oRemoved);
+            mixture.AdjustMoles(Gas.Oxygen, oxyProduced);
+            mixture.AdjustMoles(Gas.Hydrogen, hydrogenProduced);
 
-        mixture.AdjustMoles(Gas.WaterVapor, -h2oRemoved);
-        mixture.AdjustMoles(Gas.Oxygen, oxyProduced);
-        mixture.AdjustMoles(Gas.Hydrogen, hydrogenProduced);
+            var heatCap = _atmosphereSystem.GetHeatCapacity(mixture, true);
+            if (heatCap > Atmospherics.MinimumHeatCapacity)
+                mixture.Temperature = Math.Max((mixture.Temperature * heatCap) / heatCap, Atmospherics.TCMB);
+        }
 
-        var heatCap = _atmosphereSystem.GetHeatCapacity(mixture, true);
-        if (heatCap > Atmospherics.MinimumHeatCapacity)
-            mixture.Temperature = Math.Max((mixture.Temperature * heatCap) / heatCap, Atmospherics.TCMB);
+        if (initHyperNob > 0.01f && temperature < 150f)
+        {
+            var proportion = Math.Min(initHyperNob, 1.5f);
+            mixture.AdjustMoles(Gas.HyperNoblium, -proportion);
+            mixture.AdjustMoles(Gas.AntiNoblium, proportion * 0.5f);
+
+            var heatCap = _atmosphereSystem.GetHeatCapacity(mixture, true);
+            if (heatCap > Atmospherics.MinimumHeatCapacity)
+                mixture.Temperature = Math.Max((mixture.Temperature * heatCap) / heatCap, Atmospherics.TCMB);
+        }
+
+        if (initBZ > 0.01f)
+        {
+            var reactionEfficiency = Math.Min(initBZ * (1f - (float)Math.Pow(Math.E, -0.5f * temperature / Atmospherics.FireMinimumTemperatureToExist)), initBZ);
+            mixture.AdjustMoles(Gas.BZ, -reactionEfficiency);
+            mixture.AdjustMoles(Gas.Oxygen, reactionEfficiency * 0.2f);
+            mixture.AdjustMoles(Gas.Halon, reactionEfficiency * 2f);
+            var energyReleased = reactionEfficiency * Atmospherics.HalonProductionEnergy;
+
+            var heatCap = _atmosphereSystem.GetHeatCapacity(mixture, true);
+            if (heatCap > Atmospherics.MinimumHeatCapacity)
+                mixture.Temperature = Math.Max((mixture.Temperature * heatCap + energyReleased) / heatCap, Atmospherics.TCMB);
+        }
+
         _gasOverlaySystem.UpdateSessions();
     }
 }
--- Resources/Locale/en-US/atmos/gases.ftl
+++ Resources/Locale/en-US/atmos/gases.ftl
@@ -14,10 +14,18 @@ gas-tritium-abbreviation = T
 gas-water-vapor-abbreviation = H₂O
 
 # assmos gas port start
+# these are separated by the separate commits they were added in !!!
 gas-bz-abbreviation = bz
-gas-healium-abbreviation = he
+gas-healium-abbreviation = h+
 gas-nitrium-abbreviation = nit
 gas-pluoxium-abbreviation = plox
+
+gas-hyper-noblium-abbreviation = HyNo
+gas-proto-nitrate-abbreviation = PNO3
+gas-zauker-abbreviation = Z
+gas-halon-abbreviation = CBrF₃
+gas-helium-abbreviation = He
+gas-anti-noblium-abbreviation = AnNo
 
 gas-hydrogen-abbreviation = H₂
 gas-unknown-abbreviation = X
